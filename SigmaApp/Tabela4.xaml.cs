9e4b83fd0b780cb9bbb1ed44978f8b0338c31a5d
Fabio Storto Bittencourt <77692333+fabiosbit@users.noreply.github.com>
2021-11-19 19:01:50 -0300

    Calculadora Cortina
    Implementado cálculo da cortina de luz

--- SigmaApp/Tabela4.xaml.cs
+++ SigmaApp/Tabela4.xaml.cs
@@ -16,5 +16,62 @@ namespace SigmaApp
         {
             InitializeComponent();
         }
+
+        private void btnCalcularDist_Clicked(object sender, EventArgs e)
+        {
+            decimal s, d, c, t;
+
+            try
+            {
+                d = Convert.ToDecimal(entryResol.Text);
+                t = Convert.ToDecimal(entryTempo.Text);
+
+                if (t < 0)
+                {
+                    t *= -1;
+                }
+
+                if (entryResol.Text == null || entryTempo.Text == null)
+                {
+                    DisplayAlert("Erro", "Entre com os valores de T e D", "voltar");
+                }
+                else
+                {
+                    c = 8 * (d - 14);   //c não pode ser negativo.
+
+                    if (c < 0)
+                    {
+                        c = 0;
+                    }
+
+                    s = (2000 * t) + c;
+
+                    if (s >= 500)
+                    {
+                        s = (1600 * t) + c;
+                    }
+
+
+
+                    DisplayAlert("Resultado", "S = " + Convert.ToString(s) + " mm", "ok");
+                }
+
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Erro", "Valor informado é inválido", "ok");
+            }
+        }
+
+        //Verifica se o caracter digitado é um número e o apaga caso não seja.
+        private void TextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                bool numeroOk = args.NewTextValue.ToCharArray().All(x => char.IsDigit(x));
+
+                ((Entry)sender).Text = numeroOk ? args.NewTextValue : args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+            }
+        }
     }
 }
